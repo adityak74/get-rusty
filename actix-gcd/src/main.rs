818d7c7950c0bd8e230a0d2c3a803a1b822bab65
Aditya Karnam <akarnam37@gmail.com>
2022-05-27 00:19:38 -0500

    feat: serde json impl

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use serde_json::json;
 
 #[derive(Deserialize)]
 struct GcdParameters {
@@ -37,12 +38,18 @@ async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
         .body(response)
 }
 
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    web::Json(json!({ "message": format!("Hello {}!", name) }))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
             .route("/gcd", web::post().to(post_gcd))
+            .service(greet)
     });
 
     println!("Serving on localhost:3000....");
