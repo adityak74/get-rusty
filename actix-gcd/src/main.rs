acd899f82a22c7cf81cd83fa9783741e5ce1514e
Aditya Karnam <akarnam37@gmail.com>
2022-05-27 00:11:29 -0500

    feat: add gcd post endpoint

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,10 +1,48 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Cannot compute GCD for 0");
+    }
+
+    let response = 
+        format!("The GCD of numbers {} and {} \
+        is <br>{}</br>", 
+        form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
-            .service(web::resource("/").to(get_index))
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on localhost:3000....");
